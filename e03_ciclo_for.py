e6b5b85c18f8d95e5669b832246705273282bc72
AntonioCh <antoniochavezcampos94@gmail.com>
2020-11-24 22:03:01 -0600

    Ciclo for

---
+++ e03_ciclo_for.py
@@ -0,0 +1,17 @@
+lista = [1,2,3,4]
+print(lista)
+
+for elemento in lista:
+    print(elemento)
+
+nombre = 'Antonio'
+for letra in nombre: 
+    print(letra) #imprime cada letra del nombre
+
+j = [0,2,4,6,8,10,12,14,16,18,20]
+
+for p in j:
+    print(p)
+
+for p in j:
+    print(p, end = " ") #imprime en forma de lista
